6cc12436bf3f85be6e541c6561de592622f87fc1
Xandino <fandino2000@gmail.com>
2025-03-16 23:14:46 -0400

    Created src file for training / battle minigame

---
+++ digiProject/src/digiProject/Minigame.java
@@ -0,0 +1,15 @@
+/* Minigame
+ * Methods for battle / training slider minigame
+ * Author: Xandino
+*/
+package digiProject;
+import java.util.*;
+
+class Minigame {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
