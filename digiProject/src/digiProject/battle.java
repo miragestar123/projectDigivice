9a12595d637ece8a673aa7a1ac57dbddb5ef9a80
savio <savio@DESKTOP-ODM17U6>
2025-04-19 17:29:35 -0400

    Made fixes and tunes to battle logic, made battleInstance redundant

--- digiProject/src/digiProject/battle.java
+++ digiProject/src/digiProject/battle.java
@@ -3,16 +3,16 @@ import java.util.Random;
 
 public class battle extends mainDigivice {
 	public static void main(String[] args) {
-		digimon testDigimon = new digimon(7,"Tyranomon","Champion","Dragon",100,0,false,false,false,0,250,250,2);
+		digimon testDigimon = new digimon(9,"Seadramon","Champion","Aquan",100,0,false,false,false,0,280,220,2);
 		digimon testDigimon2 = new digimon(8,"Greymon","Champion","Dragon",100,0,false,false,false,0,260,240,2);
 		battle battle = new battle();
-		BattleInstance newBattle = battle.new BattleInstance(testDigimon);
+		//BattleInstance newBattle = battle.new BattleInstance(testDigimon);
 
 		//myBattler = initializeDigimon;
 		battle.startBattle(testDigimon, testDigimon2);
 	}
 	
-	private class BattleInstance{
+	/*private class BattleInstance{
 		public digimon myBattler = null;
 		public digimon theOpp = null;
 		
@@ -38,7 +38,7 @@ public class battle extends mainDigivice {
 	        // This can be modified as needed
 	        this.theOpp = new digimon(1, "Opponents Species", "Type", null, 100, 50, false, false, true, 5, 50, 100, 1);
 	    }
-	}
+	}*/
 
     public void startBattle(digimon myBattler, digimon theOpp) {
         int myHitPoints = myBattler.HP;
@@ -58,8 +58,8 @@ public class battle extends mainDigivice {
             int randomNumber = random.nextInt(100) + 1;
 
             // Check if attack is successful (e.g., hit chance)
-            if (randomNumber <= myHitrate) {  // Assuming 75% hit chance for example
-                oppHitPoints -= myBattler.getAtkPwr();
+            if (randomNumber <= myHitrate && myHitPoints > 0) {  // Assuming 75% hit chance for example
+                oppHitPoints -= (myBattler.getAtkPwr() / 2);
                 System.out.println("The attack was successful! Opponent's HP: " + oppHitPoints);
             } else {
                 System.out.println("The attack missed!");
@@ -67,8 +67,8 @@ public class battle extends mainDigivice {
 
             // Opponent attacks (similarly can be improved)
             randomNumber = random.nextInt(100) + 1;
-            if (randomNumber <= oppHitrate) {
-                myHitPoints -= theOpp.getAtkPwr();
+            if (randomNumber <= oppHitrate && oppHitPoints > 0) {
+                myHitPoints -= (theOpp.getAtkPwr() / 2);
                 System.out.println("The opponent attacks! Your HP: " + myHitPoints);
             } else {
                 System.out.println("The opponent's attack missed!");
@@ -82,7 +82,7 @@ public class battle extends mainDigivice {
                 System.out.println("The opponent's digimon has fainted. You won!");
                 break;
             } else if (turnCount == 4) {
-            	if (myHitPoints >= oppHitPoints) {
+            	if ((myHitPoints/myBattler.HP) >= (oppHitPoints/theOpp.HP)) {
                     System.out.println("You won!");
                     break;
             	} else {
